3a7810d348edfe77e16265b7b3c32d626968493f
thissideaditya <adityac486@gmail.com>
2022-05-14 14:28:03 +0530

    :construction: dynamic behaviour is working

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const addBtn = document.querySelector(".add-btn")
 const remBtn = document.querySelector(".rem-btn")
 const modalCont = document.querySelector(".modal-cont")
+const textareaCont = document.querySelector(".textarea-cont")
+const mainCont = document.querySelector(".main-cont")
+let colors = ['lightPink', 'lightGreen', 'lightBlue', 'black']
+let modalPriorityColor = colors[colors.length - 1]
 
 let addModal = false
 addBtn.addEventListener('click', function(){
@@ -24,5 +28,32 @@ allPriorityColors.forEach(function(colorElement){
             priorityColorElem.classList.remove("active")
         })
         colorElement.classList.add("active")
+        modalPriorityColor = colorElement.classList[0]
     })
 })
+
+modalCont.addEventListener("keydown", function(e){
+    let key = e.key
+    if(key == "Shift"){
+        createTicket(modalPriorityColor, textareaCont.value)
+        modalCont.style.display = "none"
+        addModal = !addModal
+        textareaCont.value = ""
+        allPriorityColors.forEach(function(colorElement){
+            colorElement.classList.remove("active")
+        })
+    }
+})
+
+
+function createTicket(ticketColor, data){
+    let ticketCont = document.createElement("div") //<div></div>
+    ticketCont.setAttribute("class", "ticket-cont")
+    ticketCont.innerHTML = `
+        <div class="ticket-color ${ticketColor} "></div>
+        <div class="ticket-id"></div>
+        <div class="task-area">${data}</div>
+    `
+
+    mainCont.appendChild(ticketCont)
+}
